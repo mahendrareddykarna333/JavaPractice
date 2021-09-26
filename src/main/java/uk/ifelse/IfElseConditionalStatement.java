7de183a114ddcc6a9c68cad2b22c69e5666dd924
Mahendra Reddy Karna <karna.mailcity@gmail.com>
2021-09-26 12:14:45 +0100

    Added if-else conditional statement

--- src/main/java/uk/ifelse/IfElseConditionalStatement.java
+++ src/main/java/uk/ifelse/IfElseConditionalStatement.java
@@ -2,11 +2,17 @@ package uk.ifelse;
 
 public class IfElseConditionalStatement {
 
+
+    public static int getLargestInThreeNumbers(int n1, int n2, int n3) {
+        int large = getLarge(n1, n2);
+        return getLarge(large, n3);
+    }
+
     public static int getLarge(int num1, int num2) {
         if (num1 < num2) {
             return num2;
         } else {
-           return num1;
+            return num1;
         }
     }
 }
